6784ad6a75755967bfc9ed8f562d550fab217cb1
nglicloud <dattatraya.jagtap@gmail.com>
2025-05-06 12:01:53 +0530

    first commit to Terraform files

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "publi_ip" {
+    value = aws_instance.example.public_ip
+    description = "Public IP of the EC2 instance"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  default = "ap-south-1"
+}
+variable "ami_id" {}
+variable "instance_type" {}
