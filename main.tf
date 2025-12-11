3f5843771a59fd95975c6b34bec95ca78131dbc9
ngli <dattatraya.jagtap@gmail.com>
2025-12-11 11:57:17 +0530

    adding ec2

--- main.tf
+++ main.tf
@@ -2,10 +2,14 @@ provider "aws" {
   region = var.region
 }
 
+resource "random_id" "id" {
+    byte_length = 8
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
-    Name = "TerraformEC2"
+    Name = "TerraformEC2 - ${random_id.id.dec}"
   }
 }
